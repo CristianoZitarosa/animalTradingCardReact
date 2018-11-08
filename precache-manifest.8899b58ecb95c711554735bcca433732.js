f65733ddfb2a440dc0eb052939055dac7e7c2a45
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2018-11-08 16:27:34 +0100

    Updates

---
+++ precache-manifest.8899b58ecb95c711554735bcca433732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c79b14aa0ae3bdf537",
+    "url": "/animalTradingCardReact/static/js/runtime~main.46c79b14.js"
+  },
+  {
+    "revision": "afcf7e0725673498006b",
+    "url": "/animalTradingCardReact/static/js/main.afcf7e07.chunk.js"
+  },
+  {
+    "revision": "2692a22608c42ef1bebd",
+    "url": "/animalTradingCardReact/static/js/1.2692a226.chunk.js"
+  },
+  {
+    "revision": "afcf7e0725673498006b",
+    "url": "/animalTradingCardReact/static/css/main.b2d597bc.chunk.css"
+  },
+  {
+    "revision": "d9baee2603cd930760de2366157ce134",
+    "url": "/animalTradingCardReact/index.html"
+  }
+];
